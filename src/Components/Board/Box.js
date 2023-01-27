49baef51fd839500e9cab007d3e2cf0180eb8465
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-27 12:40:59 -0800

    board displaying player choice on click

--- src/Components/Board/Box.js
+++ src/Components/Board/Box.js
@@ -5,15 +5,18 @@ import { useGameContext } from '../../Context/GameContext.js';
 export default function Box({ box }) {
   const { player, isActive, setIsActive, setPlayer, board, setBoard } = useGameContext();
 
-  //   function chooseSpace(player) {
-  //     const content = player;
-  //     box.content = content;
-  //     console.log(box.content, player);
-  //   }
+  function handleClick() {
+    box.content = player;
+    const newBoard = [...board];
+
+    setBoard(newBoard);
+    console.log(board);
+  }
+
   return (
     <>
       {isActive ? (
-        <button className="button" onClick={() => console.log('click', { player })}>
+        <button className="button" onClick={() => handleClick()}>
           <div className="box-container">
             {box.content && <img className="image" src={`${box.content}.png`}></img>}
           </div>

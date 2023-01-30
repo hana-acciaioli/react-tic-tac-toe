8fa010a83fa3780106a5c0b52aaf7174889a14ad
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-30 09:06:28 -0800

    added empty string check to fix bad logic where some lines were late in showing win

--- src/Context/GameContext.js
+++ src/Context/GameContext.js
@@ -20,35 +20,69 @@ const GameProvider = ({ children }) => {
 
   function checkForWin() {
     let winner = null;
-    if (board[0].content === board[1].content && board[1].content === board[2].content) {
-      return (winner = `${board[2].content}`);
-    }
-    if (board[3].content === board[4].content && board[4].content === board[5].content) {
-      return (winner = `${board[5].content}`);
-    }
-    if (board[6].content === board[7].content && board[7].content === board[8].content) {
-      return (winner = `${board[8].content}`);
-    }
-    if (board[0].content === board[3].content && board[3].content === board[6].content) {
-      return (winner = `${board[6].content}`);
-    }
-    if (board[1].content === board[4].content && board[4].content === board[7].content) {
-      return (winner = `${board[7].content}`);
-    }
-    if (board[2].content === board[5].content && board[5].content === board[8].content) {
-      return (winner = `${board[8].content}`);
-    }
-    if (board[0].content === board[4].content && board[4].content === board[8].content) {
-      return (winner = `${board[8].content}`);
-    }
-    if (board[2].content === board[4].content && board[4].content === board[6].content) {
+    if (
+      board[0].content === board[1].content &&
+      board[1].content === board[2].content &&
+      board[2].content !== ''
+    ) {
+      return (winner = board[2].content);
+    } else if (
+      board[3].content === board[4].content &&
+      board[4].content === board[5].content &&
+      board[5].content !== ''
+    ) {
+      return (winner = board[5].content);
+    } else if (
+      board[6].content === board[7].content &&
+      board[7].content === board[8].content &&
+      board[8].content !== ''
+    ) {
+      return (winner = board[8].content);
+    } else if (
+      board[0].content === board[3].content &&
+      board[3].content === board[6].content &&
+      board[6].content !== ''
+    ) {
+      return (winner = board[6].content);
+    } else if (
+      board[1].content === board[4].content &&
+      board[4].content === board[7].content &&
+      board[7].content !== ''
+    ) {
+      return (winner = board[7].content);
+    } else if (
+      board[2].content === board[5].content &&
+      board[5].content === board[8].content &&
+      board[8].content !== ''
+    ) {
+      return (winner = board[8].content);
+    } else if (
+      board[0].content === board[4].content &&
+      board[4].content === board[8].content &&
+      board[8].content !== ''
+    ) {
+      return (winner = board[8].content);
+    } else if (
+      board[2].content === board[4].content &&
+      board[4].content === board[6].content &&
+      board[6].content !== ''
+    ) {
       // eslint-disable-next-line no-unused-vars
-      return (winner = `${board[6].content}`);
-    }
+      return (winner = board[6].content);
+    } else return;
   }
+
+  //   function checkForWin() {
+  //     let winner = null;
+  //     switch (winner) {
+  //       case board[0].content === board[1].content && board[1].content === board[2].content:
+  //         winner = board[5].content;
+  //         break;
+  //     }
+  //   }
   const checkGameStatus = () => {
     if (!isActive) return;
-    const winner = checkForWin();
+    let winner = checkForWin();
     if (winner) {
       setResultsMessage(`You win ${winner}!`);
       setIsActive(false);
